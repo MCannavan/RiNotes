7d879d1efa5454a3aebfe9e90227d43492f857a3
Mark <mark.cannavan@gmail.com>
2022-08-26 19:49:31 +0200

    fixed fxml resources not being accessible from the main module

--- src/main/java/notetaker/MainLoader.java
+++ src/main/java/notetaker/MainLoader.java
@@ -11,7 +11,7 @@ import java.net.URL;
 public class MainLoader extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        URL fxmlLocation = getClass().getResource("hello-view.fxml");
+        URL fxmlLocation = getClass().getResource("/view/Homepage.fxml");
         System.out.println(fxmlLocation);
         FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
